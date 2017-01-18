3c19ccb34b93ffa73391b9fffd6a1b6bd646d1c0
jinx3035 <joebob1209@yahoo.com>
2017-01-17 18:16:18 -0600

    init class MyClockRadio

---
+++ ClockRadio/MyClockRadio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    public class MyClockRadio
+    {
+
+            public string setTime()
+            {
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                return time;
+            }
+            public string showTime(string currentTime)
+            {
+              string time = ("The time is " + currentTime);
+            return time;      
+                
+
+            }
+            
+        
+    }
+}
